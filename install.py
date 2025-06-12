88bfed0d3c817e44d560ba83c773f428ae8daca7
jesusBV20 <92217640+jesusBV20@users.noreply.github.com>
2025-06-12 10:24:25 +0200

    [build] Pin ssl_simulator to release v0.0.1

--- install.py
+++ install.py
@@ -5,5 +5,5 @@ deps_path = os.path.join(file_path, "requirements.txt")
 
 os.system("pip install -r " + deps_path)
 os.system(
-    "pip install git+https://github.com/Swarm-Systems-Lab/ssl_simulator.git@master"
+    "pip install git+https://github.com/Swarm-Systems-Lab/ssl_simulator.git@v0.0.1"
 )
